9618a982c57c372d3daae2f22917c2e64e3e861d
cilemafacan <afacan.cilem@gmail.com>
2025-02-11 18:49:41 +0300

    main loader

--- utils/dataset_loader.py
+++ utils/dataset_loader.py
@@ -1,31 +1,47 @@
+import os
+import torch
 import openslide as ops
 from torch.utils.data import Dataset
 
-class CustomDataset(Dataset):
-    def __init__(self, data, slide_dir,  transform=None):
-        self.data = data
+class CustomDatasetFromSlide(Dataset):
+    def __init__(self, dataset, slide_dir, transform=None):
+        self.dataset = dataset
         self.transform = transform
         self.slide_dir = slide_dir
-        self.slide_cache = {}
+        self.cache = []
 
     def __len__(self):
-        return len(self.data)
+        return len(self.dataset)
 
     def __getitem__(self, idx):
-        sample = self.data[idx]
-        slide_name = sample["slide_name"]
-        x = sample["x"]
-        y = sample["y"]
-        patch_size = sample["patch_size"]
-
-        if slide_name not in self.slide_cache:
-            self.slide_cache[slide_name] = ops.OpenSlide(f"{self.slide_dir}/{slide_name}")
-
-        slide = self.slide_cache[slide_name]
-        image = slide.read_region((x, y), 0, patch_size)
-        image = image.convert("RGB")
-
-        embedding = sample["embedding_vector"]
+        x = self.dataset[idx]["x"]
+        y = self.dataset[idx]["y"]
+        patch_size = self.dataset[idx]["patch_size"]
+        slide_name = self.dataset[idx]["slide_name"]
+        path = os.path.join(self.slide_dir, slide_name)
+        if path not in self.cache:
+            self.cache.append(path)
+            self.slide = ops.OpenSlide(path)
+        image = self.slide.read_region((x, y), 0, patch_size).convert("RGB")
         if self.transform:
             image = self.transform(image)
-        return image, embedding
+        embedding = self.dataset[idx]["embedding_vector"]
+        embedding = torch.tensor(embedding, dtype=torch.float32)
+        return {"image": image, "embedding": embedding}
+    
+
+class CustomDatasetFromSource(Dataset):
+    def __init__(self, dataset, transform=None):
+        self.dataset = dataset
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, idx):
+        image = self.dataset[idx]["image"]
+        if self.transform:
+            image = self.transform(image)
+        embedding = self.dataset[idx]["embedding_vector"]
+        embedding = torch.tensor(embedding, dtype=torch.float32)
+        return {"image": image, "embedding": embedding}
